a25e3c99e6a183edfb7c3e854353742942e40905
Jordan <jmillerchamp@gmail.com>
2025-12-25 17:37:47 -0600

    Created 16x16 divs

--- main.js
+++ main.js
@@ -5,35 +5,19 @@ const htmlBody = document.querySelector("body");
 const container = document.querySelector(".container");
 htmlBody.appendChild(container);
 
-// 16 divs creation
-
-for(let i = 0; i<4; i++){ // row 1
+// 16x16 Grid
+let j = 0;
+for(let i = 0; i<256; i++){ //Create 16 class tagged divs
+    if(i%16==0 && i!=0){
+        j+=1;
+    }
+    const rowNumber = "row"+j;
     let topDiv = document.createElement("div");
-    topDiv.classList.add("rowOne"); 
+    topDiv.classList.add(rowNumber); 
     topDiv.classList.add("sketch"); 
     topDiv.textContent = i+1;
     container.appendChild(topDiv);
 }
 
-for(let i = 0; i<4; i++){ // row 2
-    let topDiv = document.createElement("div");
-    topDiv.classList.add("rowTwo");
-    topDiv.classList.add("sketch"); 
-    topDiv.textContent = i+1;
-    container.appendChild(topDiv);
-}
-for(let i = 0; i<4; i++){ // row 1
-    let topDiv = document.createElement("div");
-    topDiv.classList.add("rowThree"); 
-    topDiv.classList.add("sketch"); 
-    topDiv.textContent = i+1;
-    container.appendChild(topDiv);
-}
-for(let i = 0; i<4; i++){ // row 1
-    let topDiv = document.createElement("div");
-    topDiv.classList.add("sketch"); 
-    topDiv.classList.add("rowFour"); 
-    topDiv.textContent = i+1;
-    container.appendChild(topDiv);
-}
+
 
