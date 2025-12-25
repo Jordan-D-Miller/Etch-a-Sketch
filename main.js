ab372a9aee0ec2a75ff2cf8e089a0be6ef8025de
Jordan <jmillerchamp@gmail.com>
2025-12-25 15:50:06 -0600

    Created container

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// HTML Body
+const htmlBody = document.querySelector("body"); 
+//Div container
+const container = document.querySelector(".container");
+container.textContent = "Hello World!";
+htmlBody.appendChild(container);
+
+// 16 divs creation
+
