fe1f7b2be5fac610c8d01a0ec1828ef48cc3d562
Jordan <jmillerchamp@gmail.com>
2025-12-25 17:58:10 -0600

    Fixed my 16x16 divs and correctly tagged

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ const container = document.querySelector(".container");
 htmlBody.appendChild(container);
 
 // 16x16 Grid
-let j = 0;
-for(let i = 0; i<256; i++){ //Create 16 class tagged divs
-    if(i%16==0 && i!=0){
-        j+=1;
+for(let i = 0; i<16; i++){ //Create 16 class tagged divs
+    let rowDiv = document.createElement("div");
+    const rowNumber = "row"+i;
+    rowDiv.classList.add(rowNumber);
+    container.appendChild(rowDiv);
+    for(let k = 0; k<16;k++){
+        let elementDiv = document.createElement("div");
+        elementDiv.classList.add("sketch"); 
+        elementDiv.textContent = i+1;
+        rowDiv.appendChild(elementDiv); 
     }
-    const rowNumber = "row"+j;
-    let topDiv = document.createElement("div");
-    topDiv.classList.add(rowNumber); 
-    topDiv.classList.add("sketch"); 
-    topDiv.textContent = i+1;
-    container.appendChild(topDiv);
+  
+    
 }
 
 
