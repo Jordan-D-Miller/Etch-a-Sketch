818cd7f55b29f048b16a62ea8a2215bc8be22c93
Jordan <jmillerchamp@gmail.com>
2025-12-25 18:22:26 -0600

    Changed Grid creation to a function that changes size

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 
 // HTML Body
 const htmlBody = document.querySelector("body"); 
+
 //Div container
 const container = document.querySelector(".container");
 htmlBody.appendChild(container);
 
-// 16x16 Grid
-for(let i = 0; i<16; i++){ //Create 16 class tagged divs
+function createGrid(n = 16){
+    for(let i = 0; i<n; i++){ //Create 16 Rows divs
     let rowDiv = document.createElement("div");
     const rowNumber = "row"+i;
+    //adding classes
     rowDiv.classList.add(rowNumber);
+    rowDiv.classList.add("row");
+
+    rowDiv.addEventListener("mouseover",(event)=>{
+        event.target.style.backgroundColor = "blue";
+
+    });
     container.appendChild(rowDiv);
-    for(let k = 0; k<16;k++){
+
+
+    for(let k = 0; k<n;k++){ //Create 16 divs in rows
         let elementDiv = document.createElement("div");
         elementDiv.classList.add("sketch"); 
         elementDiv.textContent = i+1;
         rowDiv.appendChild(elementDiv); 
-    }
-  
-    
+    }   
 }
+
+}
+
+
+//Default 16x16 Grid
+createGrid();
 
 
 
